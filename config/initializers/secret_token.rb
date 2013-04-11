d6f568300d25d0f94749240b4bfce75a859a5e44
marcelo <hemakesnoise@gmail.com>
2013-04-11 17:35:30 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthme::Application.config.secret_token = 'b78eabee2f84bc7d2dc2a412cc35dfed5cd40d0bb369ef872ea7d09f1b25b4a65df23112a182ff81f5f25fa41cec4f1714a9ae21229e7f143e1f2be3ec98e1cb'
