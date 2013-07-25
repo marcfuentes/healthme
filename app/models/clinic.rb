b4a03c2ae0e2660566a60c1594f05781927b7c50
marcelo <hemakesnoise@gmail.com>
2013-07-25 17:34:51 -0400

    added infowindows to clinic

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -14,4 +14,24 @@ class Clinic < ActiveRecord::Base
   address
  end
 
+
+   def gmaps4rails_infowindow
+	 "<h1>#{name}</h1>"
+      # add here whatever html content you desire, it will be displayed when users clicks on the marker
+    end
+
+def gmaps4rails_marker_picture
+ {
+  "picture" =>            "/img/#{name}.png",
+   "width" =>             35,
+   "height" =>            35,
+   
+   
+                          # If used, all other attributes skipped except "marker_anchor". This array is used as follows:
+                          # [ anchor , flat ] : flat is a boolean, anchor is an int. 
+                          # See doc here: http://google-maps-utility-library-v3.googlecode.com/svn/trunk/richmarker/docs/reference.html 
+ }
+end
+
+
 end
